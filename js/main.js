097119d6749332cb78e7043f2fa0893cf0bacf03
vadik <vadik94_1rec@bk.ru>
2021-05-17 00:34:00 +0500

    аккордеон

--- js/main.js
+++ js/main.js
@@ -94,12 +94,12 @@ const slider = () => {
     }
 
     const autoPlaySlide = () => {
-        prevSlide(slide, currentSlide, 'active');
+        prevSlide(slide, currentSlide, 'active-slider');
         currentSlide++;
         if(currentSlide >= slide.length) {
             currentSlide = 0;
         }
-        nextSlide(slide, currentSlide, 'active');
+        nextSlide(slide, currentSlide, 'active-slider');
     };
 
     setInterval(autoPlaySlide, 3000);
@@ -154,3 +154,47 @@ const upScrolling = () => {
     })
 };
 upScrolling();
+
+//аккордеон
+const accordeon = () => {
+    const accordeon = document.querySelector('.accordeon'),
+        elements = document.querySelectorAll('.element'),
+        elementContents = document.querySelectorAll('.element-content');
+
+    const open = (elem, description) => {
+        closeAll();
+        elem.classList.add('active');
+        description.style.display = 'block';
+    };
+
+    const close = (elem, description) => {
+        elem.classList.remove('active');
+        description.style.display = 'none';
+    };
+
+    const closeAll = () => {
+        elements.forEach((item) => {
+            item.classList.remove('active');
+        })
+        elementContents.forEach((item) => {
+            item.style.display = 'none';
+        })
+    };
+
+    document.body.addEventListener('click', (event) => {
+        const target = event.target;
+        if (!target.closest('.accordeon')) {
+            closeAll();
+        }
+    })
+
+    accordeon.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target.classList.contains('title')) {
+            const element = target.closest('.element');
+            const elementContent = element.querySelector('.element-content');
+            element.classList.contains('active') ? close(element, elementContent) : open(element, elementContent);
+        }
+    })
+};
+accordeon();
