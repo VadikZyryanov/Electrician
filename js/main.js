0ba937a9de688bd849f4aaa4020027c61e0f81ee
vadik <vadik94_1rec@bk.ru>
2021-05-17 11:34:40 +0500

    слайдер-карусель

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ const togglePopup = () => {
         modalCallback = document.querySelector('.modal-callback'),
         modalOverlay = document.querySelector('.modal-overlay'),
         element = document.querySelectorAll('.element.relative'),
-        buttonServices = document.querySelector('.button-services');
+        buttonServices = document.querySelector('.button-services'),
+        imgWrapper = document.querySelectorAll('.img-wrapper');
     
     const eventBody = (event) => {
         const target = event.target;
@@ -28,16 +29,29 @@ const togglePopup = () => {
         document.body.style.overflow = 'auto';   
         document.body.removeEventListener('click', eventBody);
     };
+
+    const addText = (elem, index) => {
+        const text = document.createElement('div');
+        text.classList.add('img-wrapper_text');
+        text.textContent = 'Оформить заявку';
+        text.style.display = 'block';
+        imgWrapper[index].append(text);
+        text.addEventListener('click', openPopup);
+        elem.addEventListener('mouseleave', () => {
+            text.style.display = 'none';
+        });
+    }
     
+    for (let i = 0; i < element.length; i++) {
+        element[i].addEventListener('mouseenter', (event) => {
+            const target = event.target;
+            addText(target, i);
+        });
+    }
+
     callbackBtn.addEventListener('click', openPopup);
 
     buttonServices.addEventListener('click', openPopup);
-
-    // element.forEach((el) => {
-    //     el.addEventListener('mouseover', () => {
-            
-    //     });
-    // })
 };
 togglePopup();
 
